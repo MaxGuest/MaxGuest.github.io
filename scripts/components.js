1fc03faf34076ff3dc556218f5d7488af2d9a8af
Max Guest <maxguestwork@gmail.com>
2025-12-23 19:22:39 -0600

    Made the game cards components in the javascript file.

--- scripts/components.js
+++ scripts/components.js
@@ -14,3 +14,42 @@ class THeader extends HTMLElement {
   }
 }
 customElements.define("t-header", THeader);
+
+// footer
+class TFooter extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+    <footer>
+        <div class="social-media">
+            <a href="https://i-am-guest.itch.io/">Itch.io</a>
+        </div>
+    </footer>
+    `;
+  }
+}
+customElements.define("t-footer", TFooter);
+
+// Gamecard
+class GameCard extends HTMLElement {
+  connectedCallback() {
+    const content = this.getAttribute("content");
+    const heading = this.getAttribute("heading");
+    const details = this.getAttribute("details");
+    const link = this.getAttribute("link");
+    const linkText = this.getAttribute("linkText");
+
+    this.innerHTML = `
+      <article class="card">
+        <div class="itch-widget-container">
+            ${content}
+        </div>
+        <div class="card-body">
+            <h1>${heading}</h1>
+            <p>${details}</p>
+            <a class="btn" href="${link}">${linkText}</a>
+        </div>
+    </article>
+    `;
+  }
+}
+customElements.define("game-card", GameCard);
